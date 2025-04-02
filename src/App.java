1dfe3addbca3027a2dbbbdf24f12863e0a7189c4
InnaKolor1 <ineza.permiakova@yandex.ru>
2025-04-03 02:15:46 +0300

    commit2

--- src/App.java
+++ src/App.java
@@ -4,8 +4,10 @@ public class App {
         Author author2 = new Author("Роберт", "Чалдини");
         Book book1 = new Book("Темная башня", author1, 2004);
         Book book2 = new Book("Психология влияния", author2, 1984);
-        book1.setYear(1950);
-        System.out.println("Книга: " + book1+ ", Автор: " + author1 + " Год: " + 2004);
-        System.out.println("Книга: " + book2+ ", Автор: " + author2+ "Год: " + 1984);
+        book1.setYear(2004);
+        book2.setYear(1984);
+        System.out.println("Книга: " + book1.getTitle() + ", Автор: " + book1.getAuthor().getFirstName() + " " + book1.getAuthor().getLastName() + ", Год: " + book1.getYear());
+        System.out.println("Книга: " + book2.getTitle() + ", Автор: " + book2.getAuthor().getFirstName() + " " + book2.getAuthor().getLastName() + ", Год: " + book2.getYear());
     }
-}
+    }
+
