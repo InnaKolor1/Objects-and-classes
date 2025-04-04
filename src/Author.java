a6f204fe907c8e318990248343a83373a859b1f8
InnaKolor1 <ineza.permiakova@yandex.ru>
2025-04-04 13:22:01 +0300

    commit3

--- src/Author.java
+++ src/Author.java
@@ -6,10 +6,26 @@ public class Author {
         this.firstName = firstName;
         this.lastName = lastName;
     }
+
     public String getFirstName() {
         return this.firstName;
     }
+
     public String getLastName() {
         return this.lastName;
     }
+
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return book == book;
+    }
+
+
+    public int hashCode() {
+        int result = lastName != null ? lastName.hashCode() : 0;
+        return result;
+    }
 }
+
